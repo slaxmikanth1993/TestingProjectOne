0051412c24fc752e51ce80627e06f55dafc2b860
Laxmikanth <laxmikanth.serolu@gmail.com>
2020-08-17 09:41:23 +0530

    Initial Commit

---
+++ TestingProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingProject
+//
+//  Created by Laxmikanth Reddy on 17/08/20.
+//  Copyright © 2020 SunTelematics Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
